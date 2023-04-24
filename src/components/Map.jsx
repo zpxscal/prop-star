9bb27d7c3e75986ed225d3fbc756ef1f8feacbef
pascallackner <pasc.lack@gmail.com>
2023-04-24 19:54:36 +0200

    InfoWindows fertig

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -54,7 +54,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 6 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/L8tQwk2w/marker-Haus1.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -64,7 +64,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 5 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/SxtP4Swb/marker-Haus2.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -74,7 +74,7 @@ function Map() {
       headname: "Mehrfamilienhaus",
       infotext: "Verkaufe geräumiges Mehrfamilienhaus mit jeweils 4 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/kgrVpKg0/marker-Haus3.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
 
     },
@@ -85,7 +85,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 7 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/vZ6KQFM1/marker-Haus4.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
 
     },
@@ -96,7 +96,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 5 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/yNmGK99g/marker-Haus5.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -106,7 +106,7 @@ function Map() {
       headname: "Mehrfamilienhaus",
       infotext: "Verkaufe geräumiges Mehrfamilienhaus mit jeweils 4 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/2ykyG4d4/marker-Haus6.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -116,7 +116,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 6 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/SNW24xR2/marker-Haus7.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -126,7 +126,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 5 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/769Czsv3/marker-Haus8.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -136,7 +136,7 @@ function Map() {
       headname: "Mehrfamilienhaus",
       infotext: "Verkaufe geräumiges Mehrfamilienhaus mit jeweils 5 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/V6Zf13QV/marker-Haus9.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -146,7 +146,7 @@ function Map() {
       headname: "Einfamilienhaus",
       infotext: "Verkaufe geräumiges Einfamilienhaus mit 6 Zimmern und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/bvrDbXHq/markerhaus10.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -154,60 +154,100 @@ function Map() {
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.09530619337348, lng: 15.438478944176552 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 802 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/CKmsN4k4/land1.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand2",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.114822647916526, lng: 15.409850594924189 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 1640 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/j51QVd8w/land2.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand3",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.01976697168301, lng: 15.432074253244314 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 3400 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/667F87xP/land3.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand4",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.036326403359496, lng: 15.547632407206802 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 12694 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/Jz8FYPHw/land4.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand5",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.04324830648531, lng: 15.410159591737346 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 460 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/CMjHrYG1/land5.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand6",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.1015613281271, lng: 15.504445763531765 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 1000 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/ht39QGBC/land6.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand7",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 46.96526765103392, lng: 15.487438609774248 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 6691 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/D0gGvv6P/land7.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand8",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 46.99971175355509, lng: 15.45130164874118 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 450 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/mrHFcNVD/land8.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand9",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 46.98379449160411, lng: 15.326996331245729 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 3591 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/fWKS0b7x/land9.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerLand10",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/6752/6752040.png",
       position: { lat: 47.02989279866883, lng: 15.501289104963192 },
       headname: "Grundstück",
+      infotext: "Verkaufe mein Grundstück in exzellenter Lage. Es befindet sich in einer der begehrtesten Gegenden der Stadt und bietet eine perfekte Mischung aus Ruhe und urbanem Leben. Mit einer Fläche von 1750 m2 bietet es viel Platz für den Bau eines Traumhauses oder eines Mehrfamilienhauses. Wenn Sie an diesem Angebot interessiert sind, zögern Sie nicht, mich zu kontaktieren, um weitere Informationen zu erhalten.",
+      vorschauBild: "https://i.postimg.cc/SKCJQQNY/land10.jpg",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerWohnanlage1",
@@ -216,7 +256,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 4 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/nVRg4ZWT/wohn-Anlage1.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -226,7 +266,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 5 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/G3PMhRFq/wohn-Anlage2.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -236,7 +276,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 7 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/9M3NftL7/wohn-Anlage3.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -246,7 +286,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 4 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/yxDJrczL/wohn-Anlage4.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -256,7 +296,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 3 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/xdFNGtwX/wohn-Anlage5.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -266,7 +306,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 5 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       vorschauBild: "https://i.postimg.cc/C13N7F3n/wohn-Anlage6.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -276,7 +316,7 @@ function Map() {
       headname: "Wohnanlage",
       infotext: "Verkaufe geräumige Wonanlage mit 5 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
       infotext: "https://i.postimg.cc/kGz8vwLJ/wohn-Anlage7.png",
-      email: "markler.gmail.com",
+      email: "markler@gmail.com",
       telnummer: "+43 677 6789190",
     },
     {
@@ -284,22 +324,35 @@ function Map() {
       iconUrl: "https://cdn-icons-png.flaticon.com/512/1838/1838480.png",
       position: { lat: 47.073316233380204, lng: 15.44099558606298 },
       headname: "Wohnanlage",
+      infotext: "Verkaufe geräumige Wonanlage mit 6 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
+      infotext: "https://i.postimg.cc/zXXTGhhm/wohn-Anlage8.png",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerWohnanlage9",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/1838/1838480.png",
       position: { lat: 47.105020895110705, lng: 15.420788857494795 },
       headname: "Wohnanlage",
+      infotext: "Verkaufe geräumige Wonanlage mit 4 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
+      infotext: "https://i.postimg.cc/hvGhc95p/wohn-Anlage9.png",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
     {
       id: "markerWohnanlage10",
       iconUrl: "https://cdn-icons-png.flaticon.com/512/1838/1838480.png",
       position: { lat: 47.04745462219467, lng: 15.4709866014228 },
       headname: "Wohnanlage",
+      infotext: "Verkaufe geräumige Wonanlage mit 2 Wohnungen und großem Garten. Ruhige Lage, gute Anbindung an öffentliche Verkehrsmittel, Einkaufsmöglichkeiten, Schulen und Kindergärten in der Nähe. Ideal für Familien und Paare.",
+      infotext: "https://i.postimg.cc/MKvXLmRG/wohn-Anlage10.png",
+      email: "markler@gmail.com",
+      telnummer: "+43 677 6789190",
     },
   ];
 
   return isLoaded ? (
+
     <GoogleMap
       className="z-40"
       mapContainerStyle={containerStyle}
@@ -387,9 +440,6 @@ function Map() {
         </Marker>
       ))}
 
-      {/* Ich hab mir das jetzt mal angeschaut. Sieht interessant aus. */}
-
-      {/* Ich hab mir das jetzt mal angeschaut. Sieht interessant aus. */}
     </GoogleMap>
   ) : (
     <></>
