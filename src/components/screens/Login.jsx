24a8532b9a57630f6a28209d652fb930c8f42f82
schudu <72557998+KesSaft@users.noreply.github.com>
2023-05-09 15:01:27 +0200

    connect Login to backend

--- src/components/screens/Login.jsx
+++ src/components/screens/Login.jsx
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import englisch from "../../languages/english.json";
+import TypeCheck from "../../services/typeCheck";
 
 import loginImg from "../../assets/login.jpg";
-import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("/whoami")
+      .then(async (res) => {
+        if (!res.data.emailVerified) return navigate("/emailverification");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleLogin = () => {
+    if (
+      new TypeCheck(email).isEmail() != null ||
+      new TypeCheck(password).isPassword() != null
+    ) {
+      return setError(englisch.errors.wrong_login_data);
+    }
+
+    axios
+      .post("/api/auth/login", {
+        password,
+        email,
+        remember,
+      })
+      .then((res) => {
+        if (!res.data.emailVerified) return navigate("/emailverification");
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.toJSON().message === "Network Error")
+          return setError(englisch.errors.offline);
+
+        switch (parseInt(err.response.status)) {
+          case 400:
+          case 401:
+          case 415:
+            setError(englisch.errors.wrong_login_data);
+            break;
+          case 403:
+            navigate("/");
+            break;
+          case 500:
+            setError(englisch.errors["500"]);
+            break;
+        }
+      });
+  };
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
       <div className="hidden sm:block">
@@ -13,17 +73,40 @@ export default function Login() {
       <div className="bg-gray-100 flex flex-col justify-center">
         <form className="max-w-[400px] w-full mx-auto bg-white p-4">
           <h2 className="text-4xl font-bold text-center py-6">PROP-STAR.</h2>
+          {error && <p className="text-red-600 text-center w-full">{error}</p>}
           <div className="flex flex-col py-2">
-            <label>Username or E-Mail</label>
-            <input className="border p-2" type="text" />
+            <label>E-Mail</label>
+            <input
+              className="border p-2"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setError("");
+              }}
+            />
           </div>
           <div className="flex flex-col py-2">
             <label>Password</label>
-            <input className="border p-2" type="password" />
+            <input
+              className="border p-2"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError("");
+              }}
+            />
           </div>
-          <div className="flex justify-between">
+          <div
+            className="flex justify-between cursor-pointer"
+            onClick={(e) => {
+              setRemember(!remember);
+            }}
+          >
             <p className="flex items-center">
-              <input className="mr-2" type="checkbox" /> Remember Me
+              <input className="mr-2" type="checkbox" value={remember} />{" "}
+              Remember Me
             </p>
             <div className="mr-2 flext items-center">
               {" "}
@@ -32,7 +115,11 @@ export default function Login() {
               </Link>
             </div>
           </div>
-          <button className="bg-[#00df9a] border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white">
+          <button
+            type="button"
+            className="bg-[#00df9a] border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white"
+            onClick={handleLogin}
+          >
             Sign In
           </button>
           <div style={{ display: "flex", justifyContent: "center" }}>
---
+++ src/services/typeCheck.js
@@ -0,0 +1,65 @@
+const TypeCheck = class {
+  constructor(value) {
+    this.value = value;
+  }
+
+  isPassword(min = 8, max = 30) {
+    if (!this.value) return { where: "password", error: "missing" };
+
+    if (typeof this.value !== "string")
+      return { where: "password", error: "type" };
+
+    if (this.value.length < min || this.value.length > max)
+      return { where: "password", error: "length" };
+
+    if (
+      !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!$ %^&*-]).*$/.test(
+        this.value
+      )
+    )
+      return { where: "password", error: "format" };
+  }
+
+  isUsername() {
+    if (!this.value) return { where: "username", error: "missing" };
+
+    if (typeof this.value !== "string")
+      return { where: "username", error: "type" };
+
+    if (this.value.length < 4 || this.value.length > 30)
+      return { where: "username", error: "length" };
+
+    if (this.value.includes("@"))
+      return { where: "username", error: "@symbol" };
+  }
+
+  isEmail() {
+    if (!this.value) return { where: "email", error: "missing" };
+
+    if (typeof this.value !== "string")
+      return { where: "email", error: "type" };
+
+    if (this.value.includes("@byom.de"))
+      return { where: "email", error: "fake" };
+
+    if (
+      !/^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/.test(
+        this.value
+      )
+    )
+      return { where: "email", error: "format" };
+  }
+
+  isName(where = "name") {
+    if (!this.value) return { where, error: "missing" };
+
+    if (typeof this.value !== "string") return { where, error: "type" };
+
+    if (this.value.length < 2 || this.value.length > 50)
+      return { where, error: "length" };
+
+    if (!/^[a-z-]*$/i.test(this.value)) return { where, error: "format" };
+  }
+};
+
+module.exports = TypeCheck;
